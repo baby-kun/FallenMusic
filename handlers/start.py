37f7d0365bf48a825777ef4f4c0be0c2047dedf6
baby-kun <100061813+baby-kun@users.noreply.github.com>
2022-10-09 16:03:57 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,7 +8,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(command("mstart") & filters.private & ~filters.group & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_sticker("CAACAgUAAxkBAAEENxZiNtPdibVkMsjLZrUG9NK4hotHQgAC2wEAAoM12VSdN9ujxVtnUyME")
     await message.reply_photo(
         photo=f"{START_IMG}",
         caption=f"""**━━━━━━━━━━━━━━━━━━
@@ -22,21 +21,21 @@ Usᴇ /mhelp ...
             [
                 [
                     InlineKeyboardButton(
-                        " ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ​ ", url=f"https://t.me/{bu}?startgroup=true"
+                        "Music 1", url=f"https://t.me/{bu}?startgroup=true"
                        ),
                   ],[
                     InlineKeyboardButton(
-                        " ᴏᴡɴᴇʀ ", url=f"https://t.me/{me}"
+                        "Music 2", url=f"https://t.me/{me}"
                     ),
                     InlineKeyboardButton(
-                        " sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/{SUPPORT_GROUP}"
+                        " Hinata ", url=f"https://t.me/{SUPPORT_GROUP}"
                     )
                 ],[
                     InlineKeyboardButton(
-                        " ɪɴʟɪɴᴇ ", switch_inline_query_current_chat=""
+                        " Inline ", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        " sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ ", url="https://github.com/AnonymousR1025/FallenMusic"
+                        " Owner ", url="https://github.com/baby-kun"
                     )]
             ]
        ),
